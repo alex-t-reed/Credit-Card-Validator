d76f67ebb499be9fb897ac55e9390c8d4d9bdcdb
Alex Reed <alexreed.atr@gmail.com>
2021-09-06 14:07:34 -0700

    Update ccchecker.js

--- ccchecker.js
+++ ccchecker.js
@@ -105,7 +105,7 @@ function check() {
         if (authenticity.name) {
             delete authenticity.name
         }
-        if (card.value.length == 0 || !/[^a-zA-Z]/.test(card.value)) {
+        if (card.value.length == 0 || !/[^a-zA-Z]/.test(card.value.replace(' ', ''))) {
             Swal.fire({
                 icon: 'error',
                 title: 'Oops...',
