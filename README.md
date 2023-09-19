4157acdbf1ffa4d2be78256ae835407a34cf87fe
Alex Reed <alexreed.atr@gmail.com>
2023-09-19 03:08:11 -0700

    Added support for more codes
    Updated JavaScript code to recognize and validate additional credit card types and added test cases for various card numbers

--- README.md
+++ README.md
@@ -1,17 +1,56 @@
 # Credit Card Validator
- A simple credit card validator.
+
+A simple credit card validator.
+
 ---
-[https://alex-t-reed.github.io/Credit-Card-Validator/](https://alex-t-reed.github.io/Credit-Card-Validator/)
 
->Try entering your card number to see the result, then experiment which changing one digit
+- [Demo](https://alex-t-reed.github.io/Credit-Card-Validator/)
+- [Introduction](#introduction)
+- [How to Use](#how-to-use)
+- [Understanding Credit Card Numbers](#understanding-credit-card-numbers)
+- [Validation Algorithm](#validation-algorithm)
+- [Additional Resources](#additional-resources)
+- [Contact](#contact)
 
-Credit card numbers consist of three parts:
-1. Bank identification number or issue identification number (first 6 digits)
-    - Some may be shorter than 6 digits in length, for example Visa, which is simply the number 4
-2. Individual account number (6 to 9 digits long)
-3. Check digit, added to validate authenticity based on [Luhn algorithm](https://wikipedia.org/wiki/Luhn_algorithm)
-
-You can read more about validating credit cards here:
-- [https://www.groundlabs.com/blog/anatomy-of-a-credit-card-luhn-checks-bin-ranges-data-discovery/](https://www.groundlabs.com/blog/anatomy-of-a-credit-card-luhn-checks-bin-ranges-data-discovery/)
-- [https://stevemorse.org/ssn/cc.html](https://stevemorse.org/ssn/cc.html)
 ---
+
+## Introduction
+
+This is a basic web application that allows users to validate credit card numbers. It uses the Luhn algorithm to determine the validity of the input card number. Feel free to try entering your card number and see the result. You can also experiment with changing one digit to observe the validation process.
+
+## How to Use
+
+1. Visit the [Credit Card Validator Demo](https://alex-t-reed.github.io/Credit-Card-Validator/).
+2. Input your credit card number in the provided text field.
+3. Click the "Check" button to validate the card.
+4. The application will display whether the card is valid or not, along with the card type (e.g., Visa, MasterCard).
+
+## Understanding Credit Card Numbers
+
+Credit card numbers consist of three main parts:
+
+1. **Bank Identification Number (BIN) or Issuer Identification Number (IIN)**: The first 6 digits of a credit card number. Some issuers may have shorter identification numbers, such as Visa (which starts with the digit 4).
+
+2. **Individual Account Number**: This part is typically 6 to 9 digits long and uniquely identifies the cardholder's account.
+
+3. **Check Digit**: The final digit of the credit card number, added to validate the authenticity of the entire number using the [Luhn algorithm](https://en.wikipedia.org/wiki/Luhn_algorithm).
+
+## Validation Algorithm
+
+The application uses the Luhn algorithm to validate credit card numbers. This algorithm verifies the integrity of the card number by performing a series of mathematical operations on the digits and checking if the result is divisible by 10.
+
+## Additional Resources
+
+For more information on credit card validation and anatomy, you can explore these resources:
+
+- [Anatomy of a Credit Card: Luhn Checks, BIN Ranges, Data Discovery](https://www.groundlabs.com/blog/anatomy-of-a-credit-card-luhn-checks-bin-ranges-data-discovery/)
+- [Steve Morse's Credit Card Validator](https://stevemorse.org/ssn/cc.html)
+
+## Contact
+
+- **Email**: [Alex Reed](mailto:alexreed@ucsb.edu)
+- **LinkedIn**: [Alex Reed on LinkedIn](https://www.linkedin.com/in/alextreed?trk=profile-badge)
+
+<div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="dark" data-type="VERTICAL" data-vanity="alextreed" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://www.linkedin.com/in/alextreed?trk=profile-badge">Alex Reed</a></div>
+
+Feel free to explore the [demo](https://alex-t-reed.github.io/Credit-Card-Validator/) and learn more about credit card validation!
--- js/script.js
+++ js/script.js
@@ -1,32 +1,31 @@
 let card = document.getElementsByTagName('input')[0];
 
 // Limits input to 18 digits
-card.addEventListener('keypress', function() {
+card.addEventListener('keypress', function () {
     if (card.value.length > 17) {
-        card.value.substr(0, 18);
+        card.value = card.value.substr(0, 18);
         alert('Only cards up to 18 digits are supported, do not enter spaces.');
     }
-})
+});
 
 // Removes spaces from input
 card.addEventListener('keypress', () => {
     let old = card.value;
     card.value = old.split(' ').join('');
-})
+});
 
-card.addEventListener('keypress', function() {
+card.addEventListener('keypress', function () {
     if (!/[^a-zA-Z]/.test(card.value)) {
         card.value = '';
     }
-})
+});
 
 // Stops space abuse
 card.addEventListener('keypress', () => {
     if (card.value.trim() == '') {
         card.value = '';
     }
-})
-
+});
 
 function luhnAlgo(num) {
     var sum = 0;
@@ -35,7 +34,7 @@ function luhnAlgo(num) {
             if (parseInt(num[i] * 2) < 10) {
                 sum += parseInt(num[i] * 2);
             } else {
-                let fix = parseInt(num[i] * 2)
+                let fix = parseInt(num[i] * 2);
                 let num1 = fix.toString()[0];
                 let num2 = fix.toString()[1];
                 sum += parseInt(num1) + parseInt(num2);
@@ -45,41 +44,54 @@ function luhnAlgo(num) {
         }
     }
     if (sum % 10 == 0 && num.length > 0 && num.trim() != "") {
-        return true
+        return true;
     } else {
-        return false
+        return false;
     }
 }
 
-let tests = [{
+let tests = [
+    {
         "name": "American Express",
-        "range": ['24', '37']
+        "range": ['24', '37'],
+        "length": [15]
     },
     {
         "name": "Visa",
-        "range": ['4']
+        "range": ['4'],
+        "length": [13, 16]
     },
     {
         "name": "Discover",
-        "range": ['6011', '622', '644', '645', '646', '647', '648', '649', '65']
+        "range": ['6011', '622', '644', '645', '646', '647', '648', '649', '65'],
+        "length": [16]
     },
     {
-        "name": "Mastercard",
-        "range": ['51', '52', '53', '54', '55']
+        "name": "MasterCard",
+        "range": ['51', '52', '53', '54', '55'],
+        "length": [16]
     },
     {
         "name": "Maestro",
-        "range": ['5018',
-            '5020',
-            '5038',
-            '6304',
-            '6759',
-            '6761',
-            '6762',
-            '6763'
-        ]
+        "range": ['5018', '5020', '5038', '6304', '6759', '6761', '6762', '6763'],
+        "length": [16]
+    },
+    {
+        "name": "Diners Club and Carte Blanche",
+        "range": ['36', '38', '300', '301', '302', '303', '304', '305'],
+        "length": [14]
+    },
+    {
+        "name": "JCB",
+        "range": ['2123', '1800'],
+        "length": [15]
+    },
+    {
+        "name": "JCB",
+        "range": ['3'],
+        "length": [16]
     }
-]
+];
 
 function check() {
     let authenticity = {};
@@ -97,7 +109,7 @@ function check() {
                     let cardNumber = document.getElementsByTagName('h1')[1];
                     cardNumber.classList.add('active');
                     cardNumber.innerText = card.value;
-                    break
+                    break;
                 }
             }
         }
@@ -110,20 +122,20 @@ function check() {
         cardNumber.classList.remove('active');
         authenticity.boolean = false;
         if (authenticity.name) {
-            delete authenticity.name
+            delete authenticity.name;
         }
         if (card.value.length == 0 || !/[^a-zA-Z]/.test(card.value)) {
             Swal.fire({
                 icon: 'error',
                 title: 'Oops...',
                 text: 'You need to enter in a card first.'
-            })
+            });
         } else {
             Swal.fire({
                 icon: 'error',
                 title: 'Oops...',
                 text: 'The card is invalid.'
-            })
+            });
         }
     }
     if (authenticity.boolean && typeof authenticity.name != 'string') {
@@ -131,7 +143,7 @@ function check() {
             icon: 'success',
             title: 'Valid Card',
             text: 'The card number is valid, but we do not recognize it yet.'
-        })
+        });
     }
-    console.log(authenticity)
+    console.log(authenticity);
 }
